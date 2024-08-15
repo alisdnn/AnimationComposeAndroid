6c3dff7451d8b62e418d17c98636588f44155d71
Ali Sadeghian <aliosdn@gmail.com>
2024-08-15 00:55:15 -0400

    Add Box resize animation

--- app/src/main/java/com/alisdn/animationcomposeandroid/MainActivity.kt
+++ app/src/main/java/com/alisdn/animationcomposeandroid/MainActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.alisdn.animationcomposeandroid.ui.theme.AnimationComposeAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +35,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AnimationComposeAndroidTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    AndroidBoxExample(innerPadding)
                 }
             }
         }
@@ -31,17 +44,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun AndroidBoxExample(innerPadding: PaddingValues) {
+    var largeSize by remember { mutableStateOf(false) }
+
+    val boxSize: Dp by animateDpAsState(
+        targetValue = if (largeSize) 200.dp else 100.dp
     )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .padding(16.dp)
+                .size(boxSize)
+                .background(Color.Blue)
+        ) {
+            Button(onClick = { largeSize = !largeSize }) {
+                Text(text = "Toggle size")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AnimationComposeAndroidTheme {
-        Greeting("Android")
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            AndroidBoxExample(innerPadding)
+        }
     }
 }
